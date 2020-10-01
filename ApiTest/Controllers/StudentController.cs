5bb7ebabfa4a57cefcb46af39886c8c1c8c9485c
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-10-01 10:59:53 +0200

    Add project files.

---
+++ ApiTest/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        // Props
+        private List<string> names;
+
+
+
+        // Constructor
+        public StudentController()
+        {
+            names = new List<string> { "Boby", "Dody", "Oggy", "Diddi" };
+        }
+
+
+        // GET
+        [HttpGet]
+        public async Task<IActionResult> GetStudents(int id)
+        {
+           return  Ok(names[id]);
+        }
+
+
+        // Post
+        [HttpPost("Create")]
+        public async Task<IActionResult> PostStudents(Student student)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ApiTest/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTest
+{
+    public class Student
+    {
+        public string Name;
+
+        // Constructor
+        public Student()
+        {
+
+        }
+    }
+}
